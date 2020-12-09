4c5160511f4101c273659adc42e324a63c45ad65
Rodrigo Orem <orem.rodrigo@gmail.com>
2020-12-09 23:29:47 +0000

    Update Dockerfile and build to support ARM CPU

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,25 +3,13 @@
 set -ev
 
 SCRIPT_DIR=$(dirname "$0")
+GROUP=codelab
+COMMIT=latest
 
-if [[ -z "$GROUP" ]] ; then
-    echo "Cannot find GROUP env var"
-    exit 1
-fi
-
-if [[ -z "$COMMIT" ]] ; then
-    echo "Cannot find COMMIT env var"
-    exit 1
-fi
-
-if [[ "$(uname)" == "Darwin" ]]; then
-    DOCKER_CMD=docker
-else
-    DOCKER_CMD="sudo docker"
-fi
+DOCKER_CMD=docker
 CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
-$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
+$DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven  paperinik/rpi-maven install -DskipTests package
 
 cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
 
